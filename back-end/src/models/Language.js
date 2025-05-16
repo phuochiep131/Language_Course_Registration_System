2021f2d8a04d29403c039a96a1756a3c21662710
phuochiep131 <nphhiep1301@gmail.com>
2025-05-16 19:18:32 +0700

    update languege manager

--- back-end/src/models/Language.js
+++ back-end/src/models/Language.js
@@ -4,6 +4,16 @@ const LanguageSchema = new mongoose.Schema({
   language: { type: String, required: true }
 });
 
+// const LanguageLevelSchema = new mongoose.Schema({
+//   id: { type: String, required: true},
+//   language_level: { type: String, required: true},
+// }, { _id: false });
+
+// const LanguageSchema = new mongoose.Schema({
+//   language: { type: String, required: true},
+//   levels: [LanguageLevelSchema],
+// });
+
 const Language = mongoose.model('Language', LanguageSchema);
 
 module.exports = Language
